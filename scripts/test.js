d79243912a8442061977b628c60ff9b0b0cb58f8
Nshcn <2695563464@qq.com>
2021-11-15 13:50:24 +0800

    init

---
+++ scripts/test.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    document.getElementById('settings').addEventListener('click', function () {
+        if (chrome.runtime.openOptionsPage) {
+            chrome.runtime.openOptionsPage();
+        } else {
+            window.open(chrome.runtime.getURL('options.html'));
+        }
+    });
+})
